5e2a464c813eaf43173cf9aa338f465d6e7285af
blackfulcan <brandonlimpert@gmail.com>
2020-04-26 23:04:14 -0700

    Changed Logic in Handler

--- handler.js
+++ handler.js
@@ -1,117 +1,49 @@
 'use strict';
 
-var card;
-var questionData;
-var answerData;
-var referenceData;
+var main = document.getElementById('decks');
+for(let catX = 0; catX < categories.length; ++catX) {
+    let catName = categories[catX].name;
+    
+    let catDiv = document.createElement('div');
+    catDiv.className = "flex";
 
-function setCard(card){
-    questionData = card[1].question;
-    answerData = card[1].answer;
-    referenceData = card[1].reference;
+    let catButton = document.createElement('div');
+    catButton.classList.add('button', 'm0');
+    catButton.id = 'draw-' + catName;
+    catButton.innerHTML = 'Draw<br>' + catName;
+    catDiv.appendChild(catButton);
+
+    let catQuestion = document.createElement('div');
+    catQuestion.classList.add('question', 'm0');
+    catQuestion.id = 'question-' + catName;
+    catDiv.appendChild(catQuestion);
+
+    let catAnswer = document.createElement('div');
+    catAnswer.classList.add('answer', 'm0');
+    catAnswer.id = 'answer-' + catName;
+    catDiv.appendChild(catAnswer);
+
+    let catReference = document.createElement('div');
+    catReference.classList.add('reference', 'm0');
+    catReference.id = 'reference-' + catName;
+    catDiv.appendChild(catReference);
+
+    main.appendChild(catDiv);
+
+    catButton.onclick = function() {
+        var card = draw(catX)[1];
+        catQuestion.innerHTML = card.question.toString();
+        catAnswer.innerHTML = card.answer.toString();
+        catReference.innerHTML = card.reference.toString();
+    };
 }
 
-// Handle torahButton Event
-var torahButton = document.getElementById('drawTorah');
+// example of closure function
+function createFunction(x) {
+    return function() {
+        console.log(x)
+    };
+}
 
-torahButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(0);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionTorah').innerHTML = questionData.toString();
-    document.getElementById('answerTorah').innerHTML = answerData.toString();
-    document.getElementById('referenceTorah').innerHTML = referenceData.toString();
-};
-
-
-// Handle historyButton Event
-var historyButton = document.getElementById('drawHistory');
-
-historyButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(1);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionHistory').innerHTML = questionData.toString();
-    document.getElementById('answerHistory').innerHTML = answerData.toString();
-    document.getElementById('referenceHistory').innerHTML = referenceData.toString();
-};
-
-// Handle wisdomButton Event
-var wisdomButton = document.getElementById('drawWisdom');
-
-wisdomButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(2);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionWisdom').innerHTML = questionData.toString();
-    document.getElementById('answerWisdom').innerHTML = answerData.toString();
-    document.getElementById('referenceWisdom').innerHTML = referenceData.toString();
-};
-
-// Handle prophetsButton Event
-var prophetsButton = document.getElementById('drawProphets');
-
-prophetsButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(3);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionProphets').innerHTML = questionData.toString();
-    document.getElementById('answerProphets').innerHTML = answerData.toString();
-    document.getElementById('referenceProphets').innerHTML = referenceData.toString();
-};
-
-// Handle prophetsButton Event
-var gospelsButton = document.getElementById('drawGospels');
-
-gospelsButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(4);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionGospels').innerHTML = questionData.toString();
-    document.getElementById('answerGospels').innerHTML = answerData.toString();
-    document.getElementById('referenceGospels').innerHTML = referenceData.toString();
-};
-
-// Handle actsButton Event
-var actsButton = document.getElementById('drawActs');
-
-actsButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(5);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionActs').innerHTML = questionData.toString();
-    document.getElementById('answerActs').innerHTML = answerData.toString();
-    document.getElementById('referenceActs').innerHTML = referenceData.toString();
-};
-
-// Handle paulButton Event
-var paulButton = document.getElementById('drawPaul');
-
-paulButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(6);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionPaul').innerHTML = questionData.toString();
-    document.getElementById('answerPaul').innerHTML = answerData.toString();
-    document.getElementById('referencePaul').innerHTML = referenceData.toString();
-};
-
-// Handle epistlesButton Event
-var epistlesButton = document.getElementById('drawEpistles');
-
-epistlesButton.onclick = function() {
-    // call the draw function and store the data
-    card = draw(7);
-    setCard(card);
-    // Update the HTML elements with the result of the function
-    document.getElementById('questionEpistles').innerHTML = questionData.toString();
-    document.getElementById('answerEpistles').innerHTML = answerData.toString();
-    document.getElementById('referenceEpistles').innerHTML = referenceData.toString();
-};
+var created = createFunction('hello');
+created();
